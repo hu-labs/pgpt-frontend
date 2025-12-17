c8ccb16f315ed12d599c1185cfc47ff461660c84
hisaume <hisaume@yahoo.com>
2025-12-17 21:40:43 +0000

    Add CI/CD workflow for S3 and CloudFront

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     sourcemap: true, // Ensure source maps are generated for production builds
     outDir: 'dist',
+    emptyOutDir: true,
   },
 });
